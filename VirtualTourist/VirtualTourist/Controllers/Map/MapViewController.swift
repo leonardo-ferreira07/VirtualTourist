9a9e0d9096289db053a90fbed75c7eac473e86be
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-10-26 18:43:05 +0800

    Added project structure

---
+++ VirtualTourist/VirtualTourist/Controllers/Map/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 26/10/17.
+//  Copyright © 2017 Leonardo Vinicius Kaminski Ferreira. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
